74138f611db4b67b444469769c61aa12e7815167
Tyson Lupul <sunshine@Tysons-MacBook-Pro.local>
2020-06-24 16:44:51 -0700

    App 'home' urls.py, views.py, models.py, admin.py configured.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Init(models.Model):
+    project_author = models.CharField(max_length=25)
+    project_year = models.IntegerField()
+
--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Init
 
 # Create your views here.
+def index(request):
+    title = "home app title var"
+    return render(request, 'home/index.html', {'title': title})
+
